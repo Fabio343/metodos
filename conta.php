d07d639da4eb607fb650b9ec2d94d4b06d5f2e1d
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-23 22:09:12 -0200

    adicionando classe com metodos junto a arquivo teste

---
+++ conta.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *
+ */
+ //classe conta com uma função e definindo valor inicial pra um elemento
+class conta
+{
+  public $numero;
+  public $saldo=0;
+  public $limite=100;
+  public $agencia;
+
+
+function deposita( $valor){
+  if($valor>100){
+    echo "Não é possivel depositar pois ultrapassa seu limite atual";
+  }else {
+      $this->saldo+=$valor;
+  }
+  }
+
+function saca($valor){
+  if($valor>100){
+    echo "Não é possivel sacar pois ultrapassa seu limite atual deixando sua conta negativa";
+  }else {
+      $this->saldo-=$valor;
+  }
+}
+
+function imprime($val1,$val2,$val3,$val4){
+  echo "Conta $numero";
+  echo "Saldo $saldo";
+  echo "Limite $limite";
+  echo "agencia: $agencia";
+}
+
+
+}
+
+
+
+ ?>
---
+++ testaconta.php
@@ -0,0 +1,17 @@
+<?php
+//
+declare(strict_types=1);// usado apenas apos php 7  e antes da chamada da classe para saber se o valor corresponde ao tipo que foi definido antes
+// demais php
+// if(is_float($valor)){
+//função
+//} isso deve ser na função da classe
+require 'conta.php';
+$conta=new conta();
+$conta->deposita(1000);
+$conta->saca(1000);
+$conta->imprime(1000);
+
+
+
+
+ ?>
